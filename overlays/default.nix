164b3218d1257341d1fd2347a7346ac1ed6217d0
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-09-18 18:55:59 -0300

    Maybe this will work

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,19 @@
-{ ... }: {
+{ pkgs, ... }: {
   modifications = final: prev: {
+    
     itch = prev.itch.overrideAttrs (oldAttrs: {
-      exec = "itch --no-sandbox"; 
-     });
+      desktopItems = [
+        (pkgs.makeDesktopItem {
+          name = "itch";
+          exec = "itch %U";
+          tryExec = "itch";
+          icon = "itch";
+          desktopName = "itch";
+          mimeTypes = [ "x-scheme-handler/itchio" "x-scheme-handler/itch" ];
+          comment = "Install and play itch.io games easily";
+          categories = [ "Game" ];
+        })
+      ];
+    });
   };
 }
