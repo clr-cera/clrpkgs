abef41cb75be7dc38f139306d3d7affa7a856b7f
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-09-19 19:29:18 -0300

    i think it will

--- rices/default.nix
+++ rices/default.nix
@@ -1,18 +1,4 @@
-{lib, config, ... }: 
-
-let
-  inherit (lib) mkEnableOption;
-  
-  cfg = config.services.rices;
-in
 {
-  options.services.rices = {
-    cottonCandyAwesome = mkEnableOption "Cotton Candy Rice";
-    nixCityAwesome = mkEnableOption "Nix City Rice";
-  };
-  
-  imports =
-  if cfg.cottonCandyAwesome then [ ./CottonCandy-Awesome ] else
-  if cfg.nixCityAwesome then [ ./NixCity-Awesome ] else 
-  [];
+  cottonCandy = import ./CottonCandy-Awesome;
+  nixCity = import ./NixCity-Awesome;
 }
