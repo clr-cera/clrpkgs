3c4a60d48063f42d3465745b590faecae1f3b156
clr <clrcera05@gmail.com>
2024-01-14 14:12:18 -0300

    removing again

--- overlays/default.nix
+++ overlays/default.nix
@@ -16,17 +16,5 @@
         })
       ];
     });
-    eww = prev.eww.overrideAttrs (oldAttrs: let
-      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      cargoHash = "sha256-cNGYHRV4J0YrH4kIM9Kn9i2QVKToZDJsGuBolFnyKro=";
-      buildFeatures = [
-        "wayland" "x11"
-      ];
-      nativeBuildInputs = with pkgs; [ pkg-config wrapGAppsHook ];
-
-      buildInputs = with pkgs; [ gtk3 librsvg gtk-layer-shell ];
-    });
-
   };
 }
